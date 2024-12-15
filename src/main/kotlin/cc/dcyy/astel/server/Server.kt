4c4cec4e4a4c78ab4166a7dd835d2d5f2328f0db
cooocy <cooocy@foxmail.com>
2024-12-15 15:43:40 +0800

    feat: Add LengthFieldBasedFrameDecoder.

--- src/main/kotlin/cc/dcyy/astel/server/Server.kt
+++ src/main/kotlin/cc/dcyy/astel/server/Server.kt
@@ -11,6 +11,7 @@ import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder
 
 fun main() {
     // The server only has one NioServerSocketChannel, no need to specify the boos group thread(1)
@@ -22,12 +23,11 @@ fun main() {
         serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel::class.java).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true).childHandler(object : ChannelInitializer<SocketChannel>() {
             @Throws(Exception::class)
             override fun initChannel(socketChannel: SocketChannel) {
-                // socketChannel.pipeline().addLast(LengthFieldPrepender())
+                socketChannel.pipeline().addLast(LengthFieldBasedFrameDecoder(4 * 1024 * 1024, 8, 4, 0, 0))
                 socketChannel.pipeline().addLast(MessageCodec())
                 socketChannel.pipeline().addLast(RecordRequestInboundHandler())
                 socketChannel.pipeline().addLast(AstelInboundHandler())
                 socketChannel.pipeline().addLast(AdminInboundHandler())
-
             }
         })
         val channelFuture: ChannelFuture = serverBootstrap.bind(8080).sync()
